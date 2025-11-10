10687d73f6242788bd5b2ebfa1a83d4bc56c2926
Hitesh Sharma <hitesh@Rapidos-MacBook-Pro-106.local>
2025-11-10 12:27:38 +0530

    refactored more code

--- intro/main.go
+++ intro/main.go
@@ -2,102 +2,27 @@ package main
 
 import (
 	"fmt"
-
+	"hello_world/rating"
 	"github.com/fatih/color"
 )
-
-type Rating struct {
-	ProductId string
-	Rating    int
-	Comment   []string
-	User      string
-}
 
 func main() {
 
 	color.Red("Welcome to Product Review System\n")
-	// fmt.Println(hello)
 
-	// var (
-	// 	productId string
-	// 	rating    int
-	// 	comment   []string
-	// 	user      string
-	// )
-
-	// productId = "bottle"
-	// rating = 1
-	// comment = append(comment, "Good product")
-	// comment = append(comment, "Wrong color bottle")
-	// user = "Hitesh"
-
-	// fmt.Printf("The rating for product %v given by %v is %v with comments %v \n", productId, user, rating, comment)
-
-	// map1 := make(map[string]int)
-	// map1["apple"] = 10
-	// map1["banana"] = 20
-	// map1["orange"] = 30
-	// fmt.Println(map1)
-
-	// v, exist := map1["apple"]
-	// if exist {
-	// 	fmt.Println("vaue is", v)
-	// }
-	// v1, exist1 := map1["banana"]
-	// if exist1 {
-	// 	fmt.Println("vaue is", v1)
-	// } else {
-	// 	fmt.Println("value is not found")
-	// }
-
-	// for key, value := range map1 {
-	// 	fmt.Println("key is", key, "value is", value)
-	// }
-
-	rating1 := Rating{
-		ProductId: "bottle",
-		Rating:    1,
-		Comment:   []string{"Good product", "Wrong color bottle"},
-		User:      "Hitesh",
+	// Initialize a rating aggregate and add entries
+	mapRating := &rating.Rating{}
+	e :=mapRating.AddRating("Hitesh", 1, "Good product")
+	if(e!=nil){
+		fmt.Println(e)
+	}
+	e = mapRating.AddRating("Gaurav", 3, "Good product")
+	if(e!=nil){
+		fmt.Println(e)
 	}
 
-	rating1.Comment = append(rating1.Comment, "Low price product")
-
-	printProductReview(rating1)
-
-	rating2 := Rating{
-		ProductId: "Pen",
-		Rating:    4,
-		Comment:   []string{"Good product", "Wrong color bottle"},
-		User:      "Hitesh",
-	}
-
-	printProductReview(rating2)
+	fmt.Println(mapRating)
 
 }
 
-func printMessage(rating Rating) {
-	if rating.Rating > 3 {
-		fmt.Println("Thank you for your feedback")
-	} else {
-		fmt.Println("We are sorry to hear that you are not satisfied with our product")
-	}
-}
 
-func convertRatingToStars(rating int) string {
-
-	stars := ""
-	for i := 0; i < rating; i++ {
-		stars += "*"
-	}
-	return stars
-}
-
-func printProductReview(rating Rating) {
-	fmt.Printf("---------------------------------\n")
-	fmt.Printf("The rating for product: %v\n", rating.ProductId)
-	fmt.Printf("Rating: %v \n", convertRatingToStars(rating.Rating))
-	fmt.Printf("Given By: %v \n", rating.User)
-	fmt.Printf("Comments: %v \n", rating.Comment)
-	
-}
---
+++ intro/rating/rating.go
@@ -0,0 +1,66 @@
+package rating
+
+import (
+	"fmt"
+	"time"
+)
+
+type Rating struct {
+	ratingId      int
+	averageRating float64
+	rating        []RatingDetails
+}
+
+type RatingDetails struct {
+	userId  string
+	rating  int
+	comment Comment
+}
+
+type Comment struct {
+	date       string
+	commentMsg string
+}
+
+func (RatingDetails RatingDetails) String() string {
+	return fmt.Sprintf("User: %v, Rating: %v, Comment: %v", RatingDetails.userId, RatingDetails.rating, RatingDetails.comment)
+}
+func (Comment Comment) String() string {
+	return fmt.Sprintf("Date: %v, Comment: %v", Comment.date, Comment.commentMsg)
+}
+
+func (r *Rating) AddRating(userId string, rating int, commentMsg string) error{
+
+	if(rating<0 || rating>5){
+		return fmt.Errorf("Invalid rating")
+	}
+
+	r.rating = append(r.rating, RatingDetails{
+		userId: userId,
+		rating: rating,
+		comment: Comment{
+			date:       time.Now().Format("2006-01-02 15:04:05"),
+			commentMsg: commentMsg,
+		},
+	})
+	r.averageRating = float64(SumRatings(r.rating)) / float64(len(r.rating))
+	return nil
+
+}
+
+func SumRatings(ratings []RatingDetails) int {
+	total := 0
+	for _, rd := range ratings {
+		total += rd.rating
+	}
+	return total
+}
+
+func convertRatingToStars(rating int) string {
+
+	stars := ""
+	for i := 0; i < rating; i++ {
+		stars += "*"
+	}
+	return stars
+}
